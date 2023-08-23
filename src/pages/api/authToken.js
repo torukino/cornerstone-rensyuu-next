dc7c56f5f6d8ebf4886853c5dad6349eaf37aaae
ryokino <ryo.kinoshita212@gmail.com>
2023-08-24 01:14:37 +0900

    change the credentials

--- src/pages/api/authToken.js
+++ src/pages/api/authToken.js
@@ -1,11 +1,22 @@
-import fs from 'fs';
+import dotenv from 'dotenv';
 import { GoogleAuth } from 'google-auth-library';
 
+dotenv.config();
+
 export default async function handler(req, res) {
-  // JSONファイルから認証情報を読み込む
-  // rootのdicom_rensyuu.jsonをfs.readFileSyncで読みrawCredentialsに格納
-  const rawCredentials = fs.readFileSync('dicom-rensyuu.json');
-  const credentials = JSON.parse(rawCredentials.toString());
+  // 環境変数から認証情報を読み込む
+  const credentials = {
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    auth_uri: process.env.AUTH_URI,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    project_id: process.env.PROJECT_ID,
+    token_uri: process.env.TOKEN_URI,
+    type: process.env.TYPE,
+  };
 
   // Google認証クライアントを作成
   const client = new GoogleAuth({
@@ -14,7 +25,6 @@ export default async function handler(req, res) {
   });
   // OAuth2トークンを取得
   const authToken = (await client.getAccessToken()) || '';
-  // console.log('@@@@@@@@@@@authToken', JSON.stringify(authToken));
   // トークンをレスポンスとして返す
   res.status(200).json({ token: authToken });
 }
